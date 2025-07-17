72173f0eee7de5d2e52248faac9a79ffcb60a31b
dolmario <dolmario@web.de>
2025-07-17 14:27:29 +0200

    Delete document-intelligence-system-user directory

--- document-intelligence-system-user/agents/init.py
+++
@@ -1,2 +0,0 @@
-"""Document Intelligence System Agents"""
-__version__ = "1.0.0"
--- document-intelligence-system-user/services/search/init.py
+++
@@ -1,4 +0,0 @@
-"""Search Service"""
-from .search_engine import IntelligentSearchEngine
-
-__all__ = ['IntelligentSearchEngine']
